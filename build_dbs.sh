09b73b96a02ae72181f55212768159b22e49b6dd
Kostis <kkarozos@gmail.com>
2019-06-13 18:45:38 +0300

    corrected build_dbs scripts

--- build_dbs.sh
+++ build_dbs.sh
@@ -6,7 +6,7 @@ db_capture='i2b2_capture'
 db_harmonized='i2b2_harmonized' 
 container_name="demo_postgres" # <update with postgres name container>
 
-if [ -n $1 ]; then
+if [ -z $1 ]; then
     echo "No Argument given. Exiting...."
     exit 1
 
@@ -31,13 +31,13 @@ elif [ $1 = "all" ]; then
     echo "Setting up $db_harmonized database"
     docker-compose run --rm i2b2_setup_harmonized
 
-elif [ $1 = "mipmap"]; then
+elif [ $1 = "mipmap" ]; then
 # create mipmap database
     echo "Creating $db_mipmap database"
     docker exec -it $container_name psql -U postgres -d postgres -c "DROP DATABASE IF EXISTS $db_mipmap;"
     docker exec -it $container_name psql -U postgres -d postgres -c "CREATE DATABASE $mipmap;"
 
-elif [ $1 = "capture"]; then
+elif [ $1 = "capture" ]; then
 # create and set up capture database
     echo "Creating $db_capture database"
     docker exec -it $container_name psql -U postgres -d postgres -c "DROP DATABASE IF EXISTS $db_capture;"
@@ -45,7 +45,7 @@ elif [ $1 = "capture"]; then
     echo "Setting up $db_capture database"
     docker-compose run --rm i2b2_setup
 
-elif [ $1 = "harmonize"]; then
+elif [ $1 = "harmonize" ]; then
 # create and set up harmonized database
     echo "Creating $db_harmonized database"
     docker exec -it $container_name psql -U postgres -d postgres -c "DROP DATABASE IF EXISTS $db_harmonized;"
