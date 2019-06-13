b8dfad3897b9c39f079dabf55d9ce523d55811ac
iosif <iosifspart@yahoo.gr>
2019-06-13 15:35:27 +0300

    added one step all db build and updated the readme instructions

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # ehr-datafactory-template
 
+[![AUEB](https://img.shields.io/badge/AUEB-RC-red.svg)](http://rc.aueb.gr/el/static/home) [![HBP-SP8](https://img.shields.io/badge/HBP-SP8-magenta.svg)](https://www.humanbrainproject.eu/en/follow-hbp/news/category/sp8-medical-informatics-platform/)
+
 This is a EHR DataFactory template with the folders and scripts needed to run the EHR DataFactory pipeline on a hospital server.
 
 ## Instructions
@@ -7,6 +9,15 @@ This is a EHR DataFactory template with the folders and scripts needed to run th
 Update the `docker-compose.yml` with the valid credentials and ports of the postgres container which has already been running on the hospital server.
 If the i2b2 database hasn't been created yet, edit the `build_dbs.sh` and update the `container_name` with the postgres container that is running on the server.
 Also, update the postgres docker container details in `ingestdata.sh`
+
+### DataFactory databases build
+
+**Caution!** The following creation script drop any prexisting DataFactory database.
+
+for creating all databases at once
+```shell
+$ sh build_dbs all
+```
 
 for creating the database used by MIPMapReduce
 ```shell
@@ -22,3 +33,4 @@ for creating the i2b2 harmonize database
 ```shell
 $ sh build_dbs harmonize
 ```
+
--- build_dbs.sh
+++ build_dbs.sh
@@ -6,23 +6,46 @@ db_capture='i2b2_capture'
 db_harmonized='i2b2_harmonized' 
 container_name="demo_postgres" # <update with postgres name container>
 
-if [ $1 = "mipmap"]; then
+if [ -n $1 ]; then
+    echo "No Argument given. Exiting...."
+    exit 1
+
+elif [ $1 = "all" ]; then
+    echo "Creating $db_mipmap database"
+    docker exec -it $container_name psql -U postgres -d postgres -c "DROP DATABASE IF EXISTS $db_mipmap;"
+    docker exec -it $container_name psql -U postgres -d postgres -c "CREATE DATABASE $mipmap;"
+    # create mipmap database
+    echo "Creating $db_mipmap database"
+    docker exec -it $container_name psql -U postgres -d postgres -c "DROP DATABASE IF EXISTS $db_mipmap;"
+    docker exec -it $container_name psql -U postgres -d postgres -c "CREATE DATABASE $mipmap;"
+    # create and set up capture database
+    echo "Creating $db_capture database"
+    docker exec -it $container_name psql -U postgres -d postgres -c "DROP DATABASE IF EXISTS $db_capture;"
+    docker exec -it $container_name psql -U postgres -d postgres -c "CREATE DATABASE $db_capture;"
+    echo "Setting up $db_capture database"
+    docker-compose run --rm i2b2_setup
+    # create and set up harmonized database
+    echo "Creating $db_harmonized database"
+    docker exec -it $container_name psql -U postgres -d postgres -c "DROP DATABASE IF EXISTS $db_harmonized;"
+    docker exec -it $container_name psql -U postgres -d postgres -c "CREATE DATABASE $db_harmonized;"
+    echo "Setting up $db_harmonized database"
+    docker-compose run --rm i2b2_setup_harmonized
+
+elif [ $1 = "mipmap"]; then
 # create mipmap database
     echo "Creating $db_mipmap database"
     docker exec -it $container_name psql -U postgres -d postgres -c "DROP DATABASE IF EXISTS $db_mipmap;"
     docker exec -it $container_name psql -U postgres -d postgres -c "CREATE DATABASE $mipmap;"
-fi
 
-if [ $1 = "capture"]; then
+elif [ $1 = "capture"]; then
 # create and set up capture database
     echo "Creating $db_capture database"
     docker exec -it $container_name psql -U postgres -d postgres -c "DROP DATABASE IF EXISTS $db_capture;"
     docker exec -it $container_name psql -U postgres -d postgres -c "CREATE DATABASE $db_capture;"
     echo "Setting up $db_capture database"
     docker-compose run --rm i2b2_setup
-fi
 
-if [ $1 = "harmonize"]; then
+elif [ $1 = "harmonize"]; then
 # create and set up harmonized database
     echo "Creating $db_harmonized database"
     docker exec -it $container_name psql -U postgres -d postgres -c "DROP DATABASE IF EXISTS $db_harmonized;"
