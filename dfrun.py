10abf21316cf703ed6b5d464d8d9d2d8c3ddde06
iosif <iosifspart@yahoo.gr>
2019-10-04 14:00:23 +0300

    main pipeline tested, anonymization module not working

---
+++ dfrun.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python3
+import os
+import sys
+import argparse
+import json
+import tarfile
+import logging
+import docker
+
+
+# Get the DataFactory configuration
+with open('config.json') as json_data_file:
+    config = json.load(json_data_file)
+
+# DataFactory EHR folders
+source_root = os.path.abspath(config['ehr_folders']['source'])
+dbprop_folder = os.path.abspath(config['ehr_folders']['dbproperties'])
+output_root = os.path.abspath(config['ehr_folders']['output'])
+preprocess_root = os.path.abspath(config['ehr_folders']['preprocess'])
+capture_root = os.path.abspath(config['ehr_folders']['capture'])
+harmonize_root = os.path.abspath(config['ehr_folders']['harmonize'])
+export = os.path.abspath(config['ehr_folders']['export'])
+
+
+
+# imaging etl folders (output of imaging pipeline)
+image_root = os.path.abspath(config['imaging_folders']['source'])
+
+# anonymization files
+anonym_output_root = config['anonymization_folders']['anonym_output']
+
+# get postgres container name
+container_name = config['db_docker']['container_name']
+
+client = docker.from_env()
+try:
+    pg_container = client.containers.get(container_name)
+except:
+    logging.warning('Unable to find db container %s' % container_name)
+    exit()
+# set enviroment variables
+os.environ['mipmap_pgproperties'] = dbprop_folder
+
+
+def run_docker_compose(cnfg_folder, source=None, imaging=False):
+    if source:
+        if imaging:
+            source_path = os.path.join(image_root, source)
+        else:
+            source_path = os.path.join(source_root, source)
+        os.environ['mipmap_source'] = source_path
+    os.environ['mipmap_map'] = cnfg_folder
+    os.system('docker-compose run mipmap_etl')
+
+
+def copy_to(src, dst):
+    tar = tarfile.open(src + '.tar', mode='w')
+    try:
+        tar.add(src, arcname=os.path.basename(src))
+    finally:
+        tar.close()
+    data = open(src + '.tar', 'rb').read()
+    pg_container.put_archive(os.path.dirname(dst), data)
+    os.remove(src + '.tar')
+
+
+def get_from(src, dst):
+    bits, stats = pg_container.get_archive(src)
+    tmp_tar_path = os.path.join(dst, 'tmp.tar')
+    f = open(tmp_tar_path, 'wb')
+    for chunk in bits:
+        f.write(chunk)
+    f.close
+    tar = tarfile.open(tmp_tar_path)
+    tar.extractall(dst)
+    os.remove(tmp_tar_path)
+
+
+def export_csv(output_folder, csv_name, sql_script):
+    db_user = config['db_docker']['postgres_user']
+    i2b2_name = config['db_docker']['harmonize_db']
+    logging.info('Performing EHR DataFactory export step')
+    # try to delete any existing flattened csv in postgres container
+    try:
+        cmd_rm_csv = 'rm -rf /tmp/%s' % csv_name
+        pg_container.exec_run(cmd_rm_csv)
+    except:
+        pass
+    # Copy the sql script to the postgres container
+    sql_script_path = os.path.join(export, sql_script)
+    copy_to(sql_script_path, '/tmp/')
+    print(os.path.abspath(os.path.dirname(__file__)))
+    # run the sql script
+    cmd_sql = 'psql -q -U %s -d %s -f /tmp/%s' % (db_user, i2b2_name, sql_script)
+    pg_container.exec_run(cmd_sql)
+    # check if the output folder exist, create it otherwise
+    if not os.path.exists(output_folder):
+        try:
+            os.makedirs(output_folder)
+        except OSError:
+            print('Creation of the output directory %s failed' % output_folder)
+        else:
+            print('Output directory %s is created' % output_folder)
+    # copy the flatten csv to the Data Factory output folder
+    docker_csv_path = '/tmp/%s' % csv_name
+    get_from(docker_csv_path, output_folder)
+
+
+def anonymize_db(output_folder, csv_name):
+    db_user = config['db_docker']['postgres_user']
+    i2b2_source = config['db_docker']['harmonize_db']
+    i2b2_anonym = config['db_docker']['anonymized_db']
+    anonym_sql = config['anonymization_files']['anonymization_sql']
+    pivoting_sql = config['anonymization_files']['pivoting_sql']
+    # drop the existing anonymized db and create a new one
+    cmd_drop_db = 'psql -U %s -d postgres -c "DROP DATABASE IF EXIST %s;"' % (db_user, i2b2_anonym)
+    pg_container.exec_run(cmd_drop_db)
+    cmd_create_db = 'psql -U %s -d postgres -c "CREATE DATABASE %s WITH TEMPLATE %s;"' % (db_user, i2b2_anonym, i2b2_source)
+    pg_container.exec_run(cmd_create_db)
+    # copy the anonymization sql to the postgres container
+    sql_script_path = os.path.join(export, anonym_sql)
+    print(sql_script_path)
+    copy_to(sql_script_path, '/tmp/')
+    # run the anonymization sql script
+    cmd_sql = 'psql -q -U %s -d %s -f /tmp/%s' % (db_user, i2b2_anonym, anonym_sql)
+    pg_container.exec_run(cmd_sql)
+    export_csv(output_folder, csv_name, pivoting_sql)
+
+
+# run_docker_compose('./preprocess_step')
+# test_output = output_root+'/'
+# export_csv('./output/')
+# copy_to('./export_step/pivot_i2b2_MinDate_NEW19_a.sql', '/tmp/sql_script.sql')
+
+
+def main():
+    parser = argparse.ArgumentParser(description='EHR datafactory cli')
+    parser.add_argument('step', choices=['preprocess', 'capture',
+                                         'harmonize', 'export',
+                                         'imaging', 'anonymize'],
+                        help='select datafactory step')
+    parser.add_argument('-s', '--source', type=str,
+                        help='input folder for ehr csv file')
+    parser.add_argument('-c', '--config', type=str,
+                        help='folder containing step\'s configuration files')
+    parser.add_argument('-o', '--output', type=str,
+                        help='DF output folder for the flat csv')
+
+    args = parser.parse_args(sys.argv[1:])
+    if args.step == 'preprocess':
+        config_folder = os.path.join(preprocess_root, args.config)
+        run_docker_compose(config_folder, source=args.source)
+    elif args.step == 'capture':
+        config_folder = os.path.join(capture_root, args.config)
+        run_docker_compose(config_folder, source=args.source)
+    elif args.step == 'harmonize':
+        config_folder = os.path.join(harmonize_root, args.config)
+        run_docker_compose(config_folder)
+    elif args.step == 'imaging':
+        image_mapping = os.path.abspath(config['imaging_folders']['mapping'])
+        run_docker_compose(image_mapping, source=args.source,
+                           imaging=True)
+    elif args.step == 'anonymize':
+        flat_anonym_csv = config['anonymization_files']['anonymized_csv']
+        output_folder = os.path.join(anonym_output_root, args.output)
+        anonymize_db(output_folder, flat_anonym_csv)
+    elif args.step == 'export':
+        flat_csv_name = config['ehr_files']['export_csv']
+        pivoting_sql = config['ehr_files']['pivoting_sql']
+        output_folder = os.path.join(output_root, args.output)
+        export_csv(output_folder, flat_csv_name, pivoting_sql)
+
+if __name__ == '__main__':
+    main()
--- export_step/pivot_i2b2_MinDate_NEW19_a_fed.sql
+++ export_step/pivot_i2b2_MinDate_NEW19_a_fed.sql
@@ -29,8 +29,8 @@ countInserts := 0;
 table_name := 'new_table';
 query := 'CREATE TABLE ' || table_name || '( "subjectcode" text, "subjectageyears" text, "subjectage" text, "gender" text, "_3rdventricle" text, "_4thventricle" text, "rightaccumbensarea" text, "leftaccumbensarea" text, "rightamygdala" text, "leftamygdala" text, "brainstem" text, "rightcaudate" text, "leftcaudate" text, "rightcerebellumexterior" text, "leftcerebellumexterior" text, "rightcerebellumwhitematter" text, "leftcerebellumwhitematter" text, "rightcerebralwhitematter" text, "leftcerebralwhitematter" text, "csfglobal" text, "righthippocampus" text, "lefthippocampus" text, "rightinflatvent" text, "leftinflatvent" text, "rightlateralventricle" text, "leftlateralventricle" text, "rightpallidum" text, "leftpallidum" text, "rightputamen" text, "leftputamen" text, "rightthalamusproper" text, "leftthalamusproper" text, "rightventraldc" text, "leftventraldc" text, "opticchiasm" text, "cerebellarvermallobulesiv" text, "cerebellarvermallobulesvivii" text, "cerebellarvermallobulesviiix" text, "leftbasalforebrain" text, "rightbasalforebrain" text, "rightacgganteriorcingulategyrus" text, "leftacgganteriorcingulategyrus" text, "rightainsanteriorinsula" text, "leftainsanteriorinsula" text, "rightaorganteriororbitalgyrus" text, "leftaorganteriororbitalgyrus" text, "rightangangulargyrus" text, "leftangangulargyrus" text, "rightcalccalcarinecortex" text, "leftcalccalcarinecortex" text, "rightcocentraloperculum" text, "leftcocentraloperculum" text, "rightcuncuneus" text, "leftcuncuneus" text, "rightententorhinalarea" text, "leftententorhinalarea" text, "rightfofrontaloperculum" text, "leftfofrontaloperculum" text, "rightfrpfrontalpole" text, "leftfrpfrontalpole" text, "rightfugfusiformgyrus" text, "leftfugfusiformgyrus" text, "rightgregyrusrectus" text, "leftgregyrusrectus" text, "rightioginferioroccipitalgyrus" text, "leftioginferioroccipitalgyrus" text, "rightitginferiortemporalgyrus" text, "leftitginferiortemporalgyrus" text, "rightliglingualgyrus" text, "leftliglingualgyrus" text, "rightlorglateralorbitalgyrus" text, "leftlorglateralorbitalgyrus" text, "rightmcggmiddlecingulategyrus" text, "leftmcggmiddlecingulategyrus" text, "rightmfcmedialfrontalcortex" text, "leftmfcmedialfrontalcortex" text, "rightmfgmiddlefrontalgyrus" text, "leftmfgmiddlefrontalgyrus" text, "rightmogmiddleoccipitalgyrus" text, "leftmogmiddleoccipitalgyrus" text, "rightmorgmedialorbitalgyrus" text, "leftmorgmedialorbitalgyrus" text, "rightmpogpostcentralgyrusmedialsegment" text, "leftmpogpostcentralgyrusmedialsegment" text, "rightmprgprecentralgyrusmedialsegment" text, "leftmprgprecentralgyrusmedialsegment" text, "rightmsfgsuperiorfrontalgyrusmedialsegment" text, "leftmsfgsuperiorfrontalgyrusmedialsegment" text, "rightmtgmiddletemporalgyrus" text, "leftmtgmiddletemporalgyrus" text, "rightocpoccipitalpole" text, "leftocpoccipitalpole" text, "rightofugoccipitalfusiformgyrus" text, "leftofugoccipitalfusiformgyrus" text, "rightopifgopercularpartoftheinferiorfrontalgyrus" text, "leftopifgopercularpartoftheinferiorfrontalgyrus" text, "rightorifgorbitalpartoftheinferiorfrontalgyrus" text, "leftorifgorbitalpartoftheinferiorfrontalgyrus" text, "rightpcggposteriorcingulategyrus" text, "leftpcggposteriorcingulategyrus" text, "rightpcuprecuneus" text, "leftpcuprecuneus" text, "rightphgparahippocampalgyrus" text, "leftphgparahippocampalgyrus" text, "rightpinsposteriorinsula" text, "leftpinsposteriorinsula" text, "rightpoparietaloperculum" text, "leftpoparietaloperculum" text, "rightpogpostcentralgyrus" text, "leftpogpostcentralgyrus" text, "rightporgposteriororbitalgyrus" text, "leftporgposteriororbitalgyrus" text, "rightppplanumpolare" text, "leftppplanumpolare" text, "rightprgprecentralgyrus" text, "leftprgprecentralgyrus" text, "rightptplanumtemporale" text, "leftptplanumtemporale" text, "rightscasubcallosalarea" text, "leftscasubcallosalarea" text, "rightsfgsuperiorfrontalgyrus" text, "leftsfgsuperiorfrontalgyrus" text, "rightsmcsupplementarymotorcortex" text, "leftsmcsupplementarymotorcortex" text, "rightsmgsupramarginalgyrus" text, "leftsmgsupramarginalgyrus" text, "rightsogsuperioroccipitalgyrus" text, "leftsogsuperioroccipitalgyrus" text, "rightsplsuperiorparietallobule" text, "leftsplsuperiorparietallobule" text, "rightstgsuperiortemporalgyrus" text, "leftstgsuperiortemporalgyrus" text, "righttmptemporalpole" text, "lefttmptemporalpole" text, "righttrifgtriangularpartoftheinferiorfrontalgyrus" text, "lefttrifgtriangularpartoftheinferiorfrontalgyrus" text, "rightttgtransversetemporalgyrus" text, "leftttgtransversetemporalgyrus" text, "montrealcognitiveassessment" text, "minimentalstate" text, "agegroup" text, "handedness" text, "updrstotal" text, "updrshy" text, "adnicategory" text, "edsdcategory" text, "ppmicategory" text, "alzheimerbroadcategory" text, "parkinsonbroadcategory" text, "neurogenerativescategories" text, "dataset" text, "apoe4" text, "rs3818361_t" text, "rs744373_c" text, "rs190982_g" text, "rs1476679_c" text, "rs11767557_c" text, "rs11136000_t" text, "rs610932_a" text, "rs3851179_a" text, "rs17125944_c" text, "rs10498633_t" text, "rs3764650_g" text, "rs3865444_t" text, "rs2718058_g" text, "fdg" text, "pib" text, "av45" text';
 -- collect all hospital specific concept_cds in order to obtain the available columns
---raise notice '1. Here is the CREATE TABLE query: %', query;
-for concept, valtype in 
+raise notice '1. Here is the CREATE TABLE query: %', query;
+/*for concept, valtype in 	-- UPDATE: commenting out this part... cause we said that in the anonymized federation dataset we ONLY want the CDEs...
 	select distinct on (concept_cd) concept_cd, valtype_cd 
 	from observation_fact
 loop
@@ -44,18 +44,18 @@ CASE
 END CASE;
 if query = '' then
 	query := query || '"' || concept || '"' || type_name ;
-else
+else		
 	-- if the concept_cd has not been inserted before
 	if query not like ('%"' || concept || '"%') then
 		-- add it in the create statement
 		query := query || ',' || '"' || concept || '"' || type_name ;
 	end if;
 end if;
-end loop;
---raise notice '2. Here is the CREATE TABLE query: %', query;
+end loop;*/
+raise notice '2. Here is the CREATE TABLE query: %', query;
 -- add primary key
 query := query || ', PRIMARY KEY (subjectcode))';
---raise notice '3. And here it is now: %', query;
+raise notice '3. And here it is now: %', query;
 -- execute statement
 execute format('DROP TABLE IF EXISTS ' || table_name);
 --raise notice '4. And here it is now: %', query;
@@ -120,19 +120,19 @@ loop
 		observation_fact as v 
 	where foo.patient_num = v.patient_num and foo.mindate = v.start_date and concept_cd = concept)
 	loop
-		--raise notice 'valtype - char_val - num_val -patient_ide(subjectcode)-: % - % - % -%-', valtype, char_val, num_val, subjectcode;
+		raise notice 'valtype - char_val - num_val -patient_ide(subjectcode)-: % - % - % -%-', valtype, char_val, num_val, subjectcode;
 		CASE
 		    WHEN 'T'=valtype -- the problem is when valtype is NOT T and num_val IS NULL..... 
 		    THEN
 			IF char_val IS NULL THEN
 				char_val = '';
 			END IF;
-			execute format('update ' || table_name  || ' set ' || '"' || concept || '" = ''' ||  char_val || ''' where subjectcode = '' || subjectcode || ''');		    
+			execute format('update ' || table_name  || ' set ' || '"' || concept || '" = ''' ||  char_val || ''' where subjectcode = ''' || subjectcode || '''');		    
 			countUpdates=countUpdates+1;
 		    WHEN 'N'=valtype
 		    THEN	
 			IF num_val IS NOT NULL THEN
-				execute format('UPDATE ' || table_name  || ' set ' || '"' || concept || '" = ' ||  num_val || ' where subjectcode = '' || subjectcode || ''');		
+				execute format('UPDATE ' || table_name  || ' set ' || '"' || concept || '" = ' ||  num_val || ' where subjectcode = ''' || subjectcode || '''');
 			countUpdates=countUpdates+1;
 			END IF;		
 			
